3b5f16a3ac7c3edddb87456a4c197d00780ec9f1
Fernando José <fernando.tecnicoinfo7@gmail.com>
2025-12-14 19:49:20 -0300

    feat: implement caesar_cipher function for text encryption

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,12 +1,11 @@
-def caesar_cipher(mensage, shift)
-  caracter_to_interger = mensage.codepoints
-  caracter_to_interger.map! do |char_code|
+def caesar_cipher(message, shift)
+  shift %= 26
+  message.codepoints.map do |char_code|
     if char_code.between?(65, 90) || char_code.between?(97, 122)
       base = char_code <= 90 ? 65 : 97
       char_code += shift
       char_code = (char_code - base) % 26 + base
     end
     char_code.chr
-  end
-  caracter_to_interger.join('')
+  end.join
 end
