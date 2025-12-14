5b17d1f74275d72ecf09c2e92ea18030ce41eb36
Fernando José <fernando.tecnicoinfo7@gmail.com>
2025-12-14 19:49:34 -0300

    feat: adicionar testes para a função caesar_cipher

--- lib/spec/caesar_cipher.spec.rb
+++ lib/spec/caesar_cipher.spec.rb
@@ -1,27 +1,32 @@
-require 'spec_helper'
 require_relative '../caesar_cipher'
 
-RSpec.describe 'caesar_cipher' do
-  describe 'DGiven a message and shift amount' do
-    it 'Return the encrypted message with uppercase letters' do
+RSpec.describe '#caesar_cipher' do
+  describe 'when given a message and a shift amount' do
+    it 'returns encrypted uppercase letters' do
       expect(caesar_cipher('MIDDLE OUTZ', 9)).to eq('VRMMUN XDCI')
     end
-    it 'Return the encrypted message with positive shift' do
+
+    it 'returns encrypted message with positive shift' do
       expect(caesar_cipher('What a string!', 5)).to eq('Bmfy f xywnsl!')
     end
-    it 'Return the encrypted message with negative shift' do
+
+    it 'supports negative shifts' do
       expect(caesar_cipher('What a string!', -5)).to eq('Rcvo v nomdib!')
     end
-    it 'Return the encrypted message without shift' do
+
+    it 'returns the same message with zero shift' do
       expect(caesar_cipher('D3q4', 0)).to eq('D3q4')
     end
-    it 'Return the encrypted message with characters in between' do
+
+    it 'normalizes large shifts' do
       expect(caesar_cipher('www.abc.xy', 87)).to eq('fff.jkl.gh')
     end
-    it 'Return the encrypted message with numbers in between' do
+
+    it 'ignores numbers' do
       expect(caesar_cipher('159357lcfd', 98)).to eq('159357fwzx')
     end
-    it 'Return the encrypted message with spaces in between' do
+
+    it 'preserves case and spaces' do
       expect(caesar_cipher('middle Outz', 9)).to eq('vrmmun Xdci')
     end
   end
